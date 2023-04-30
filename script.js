5ae6b10fc3a79cc3743ebce36853619a3896ab76
Akhila <akhilarb4603@gmail.com>
2023-04-30 21:26:48 +0530

    Added questions for the quiz and its answers

---
+++ script.js
@@ -0,0 +1,92 @@
+const questions=[
+    {
+        question:"One light year is approximately equal to",
+        answers:[
+            {text:"9.4 billion km",correct:false},
+            {text:"9.4 million km",correct:false},
+            {text:"9.4 trillion km",correct:true},
+            {text:"94 trillion km",correct:false},
+        ]
+    },
+    {
+        question:"The location of sun on the first day of spring is called",
+        answers:[
+            {text:"zenith",correct:false},
+            {text:"vernal equinox",correct:false},
+            {text:"horizon",correct:true},
+            {text:"equator",correct:false},
+        ]
+    },
+    {
+        question:"A cloud of stars set at 1 million light years distance is called",
+        answers:[
+            {text:"comet",correct:false},
+            {text:"debris",correct:false},
+            {text:"Andromeda galaxy",correct:false},
+            {text:"Local group",correct:false},
+        ]
+    },
+    {
+        question:"Which of the following planets has a lesser rotation time than the Earth?",
+        answers:[
+            {text:"Jupiter",correct:true},
+            {text:"Mars",correct:false},
+            {text:"Mercury",correct:false},
+            {text:"Venus",correct:false},
+        ]
+    },
+    {
+        question:"The name of the galaxy in which the Earth is a planet is",
+        answers:[
+            {text:"Andromeda",correct:false},
+            {text:"Ursa Major",correct:false},
+            {text:"Ursa Minor",correct:false},
+            {text:"Milky Way",correct:true},
+        ]
+    },
+    {
+        question:"A star of which colour can be said to be the coolest?",
+        answers:[
+            {text:"Yellow",correct:false},
+            {text:"Blue",correct:false},
+            {text:"Red",correct:true},
+            {text:"Black",correct:false},
+        ]
+    },
+    {
+        question:"In order to know the time, the astronauts orbiting in an earth satellite should use:",
+        answers:[
+            {text:"A watch having mainspring to keep it going",correct:true},
+            {text:"A pendulum clock",correct:false},
+            {text:"Either a pendulum clock or a watch",correct:false},
+            {text:"An alarm clock",correct:false},
+        ]
+    },
+    {
+        question:"The Black Hole is:",
+        answers:[
+            {text:"A dark hollow cavity",correct:false},
+            {text:"A massive collapsing star",correct:true},
+            {text:"The other side of the moon",correct:false},
+            {text:"The other side of the sun",correct:false},
+        ]
+    },
+    {
+        question:"Which planet has the solar system's highest mountain Olympus Mons?",
+        answers:[
+            {text:"Jupiter",correct:false},
+            {text:"Uranus",correct:false},
+            {text:"Venus",correct:false},
+            {text:"Mars",correct:true},
+        ]
+    },
+    {
+        question:"Which planet looks reddish in the night sky?",
+        answers:[
+            {text:"Jupiter",correct:false},
+            {text:"Saturn",correct:false},
+            {text:"Mars",correct:false},
+            {text:"Mercury",correct:false},
+        ]
+    }
+];
