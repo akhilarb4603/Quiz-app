de4d03546b5e15cf0cbaa7ceeb3869161e1376f7
Akhila <akhilarb4603@gmail.com>
2023-05-01 13:54:03 +0530

    added functionality for the quiz

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ const questions=[
     {
         question:"The location of sun on the first day of spring is called",
         answers:[
-            {text:"zenith",correct:false},
-            {text:"vernal equinox",correct:false},
-            {text:"horizon",correct:true},
-            {text:"equator",correct:false},
+            {text:"Zenith",correct:false},
+            {text:"Vernal equinox",correct:true},
+            {text:"Horizon",correct:false},
+            {text:"Equator",correct:false},
         ]
     },
     {
         question:"A cloud of stars set at 1 million light years distance is called",
         answers:[
-            {text:"comet",correct:false},
-            {text:"debris",correct:false},
-            {text:"Andromeda galaxy",correct:false},
-            {text:"Local group",correct:false},
+            {text:"Comet",correct:false},
+            {text:"Debris",correct:false},
+            {text:"Andromeda Galaxy",correct:true},
+            {text:"Local Group",correct:false},
         ]
     },
     {
@@ -85,8 +85,90 @@ const questions=[
         answers:[
             {text:"Jupiter",correct:false},
             {text:"Saturn",correct:false},
-            {text:"Mars",correct:false},
+            {text:"Mars",correct:true},
             {text:"Mercury",correct:false},
         ]
     }
 ];
+
+const questionElement=document.getElementById("question");
+const answerButtons=document.getElementById("answer-buttons");
+const nextButton=document.getElementById("next-btn");
+
+let currentQuestionIndex=0;
+let score=0;
+
+function startQuiz(){
+    currentQuestionIndex=0;
+    score=0;
+    nextButton.innerHTML="Next";
+    showQuestion();
+}
+function showQuestion(){
+    resetState();
+    let currentQuestion=questions[currentQuestionIndex];
+    let questionNo=currentQuestionIndex+1;
+    questionElement.innerHTML=questionNo+". "+currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer =>{
+        const button=document.createElement("button");
+        button.innerHTML=answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct=answer.correct;
+        }
+        button.addEventListener("click",selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display="none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn=e.target;
+    const isCorrect=selectedBtn.dataset.correct==="true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button=>{
+        if(button.dataset.correct==="true"){
+            button.classList.add("correct");
+        }
+        button.disabled=true;
+    });
+    nextButton.style.display="block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML=`You scored ${score} out of ${questions.length}!;`
+    nextButton.innerHTML="Play Again";
+    nextButton.style.display="block";
+}
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex<questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex<questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+})
+startQuiz();
